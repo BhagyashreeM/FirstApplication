ec727ab5bdd6e32669a20a23ed0fd23fe1206172
Bhagyashree <bhagyashree@eggheradcreative.com>
2016-09-06 14:59:25 +0530

    minor changes

--- app/src/main/java/com/example/tapti/firstapplication/com/example/tapti/firstapplication/materialDesign/MaterialDesignActivity.java
+++ app/src/main/java/com/example/tapti/firstapplication/com/example/tapti/firstapplication/materialDesign/MaterialDesignActivity.java
@@ -1,11 +1,18 @@
 package com.example.tapti.firstapplication.com.example.tapti.firstapplication.materialDesign;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.TabLayout;
-import com.example.
+import com.example.tapti.firstapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MaterialDesignActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -19,10 +26,48 @@ public class MaterialDesignActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
 
-       viewPager = findViewById(R.id.viewpager);
+       viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
         tabs = (TabLayout) findViewById(R.id.tabs);
-        tabs.addTab(tabs.newTab().setText("First tab"));
-        tabs.addTab(tabs.newTab().setText("Second tab"));
-        tabs.addTab(tabs.newTab().setText("Third tab"));
+        tabs.setupWithViewPager(viewPager);
+//        tabs.addTab(tabs.newTab().setText("First tab"));
+//        tabs.addTab(tabs.newTab().setText("Second tab"));
+//        tabs.addTab(tabs.newTab().setText("Third tab"));
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        RecyclerView.Adapter adapter = new RecyclerView.Adapter(getSupportFragmentManager());
+        adapter.addFragment(new ListContentFragment(), "List");
+        adapter.addFragment(new TileContentFragment(), "Tile");
+        adapter.addFragment(new CardContentFragment(), "Card");
+        viewPager.setAdapter(adapter);
+
+        class Adapter extends FragmentPagerAdapter {
+            private final List<Fragment> mFragmentList = new ArrayList<>();
+            private final List<String> mFragmentTitleList = new ArrayList<>();
+
+            public Adapter(FragmentManager manager) {
+                super(manager);
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                return mFragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragmentList.size();
+            }
+
+            public void addFragment(Fragment fragment, String title) {
+                mFragmentList.add(fragment);
+                mFragmentTitleList.add(title);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mFragmentTitleList.get(position);
+            }
+        }
     }
 }
